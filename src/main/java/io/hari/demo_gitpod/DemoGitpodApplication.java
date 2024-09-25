a5d44a7c442b81460564b2d63c2ee8b1bbc6012c
Hariom Yadav <hoc.yadav@gmail.com>
2024-09-25 18:52:04 +0000

    first commit

---
+++ src/main/java/io/hari/demo_gitpod/DemoGitpodApplication.java
@@ -0,0 +1,13 @@
+package io.hari.demo_gitpod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitpodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitpodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hari/demo_gitpod/DemoGitpodApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hari.demo_gitpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitpodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
